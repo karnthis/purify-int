37ec38a9fad1bd9e9282ce8f07f25a9c5df3a0dd
Erin <erin@atomicbear.com>
2019-03-29 22:18:34 -0500

    added more functions

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-
-const asArrayInt = (arr, f) => {
-	return arr.map(x => (f == 'R') ? asIntR(x) : asInt(x))
+const asArrayInt = (arr, mode) => {
+	return arr.map(x => {
+		if (mode == 'R') return asIntR(x)
+		if (mode == 'N') return asIntIn(x)
+		if (mode == 'K') return (asIntN(x) === null) ? x : asIntN(x)
+		return asInt(x)
+	})
 }
 
-const asInt = (x) => {
+const asInt = (x, fb = 0) => {
 	const madeInt = parseInt(x)
-	return (isNaN(madeInt)) ? 0 : madeInt
+	const madeFb = parseInt(fb)
+	return (isNaN(madeInt)) ? (isNaN(madeFb)) ? 0 : madeFb : madeInt
 }
 
 const asIntR = (x) => {
 	return asInt(x) || Math.ceil(Math.random() * Date.now())
 }
 
+const asIntF = (x, y) => {
+	return asInt(x, y)
+}
 
-module.exports = { asArrayInt, asInt, asIntR }
+const asIntIn = (x) => {
+	return (asIntN(x) === null) ? x : asIntN(x)
+}
+
+const asIntN = (x) => {
+	const madeInt = parseInt(x)
+	return (isNaN(madeInt)) ? null : madeInt
+}
+
+
+module.exports = { asArrayInt, asInt, asIntR, asIntF, asIntIn, asIntN }
